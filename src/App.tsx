95f3f281fdaa036a3b960f2ad447cab8ada16213
Aiko Hoshi <aihokoshi@gmail.com>
2022-09-15 14:41:49 +0200

    Dimych Lesson 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {TaskType, Todolist} from './Todolist';
+import {v1} from 'uuid';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let tasks1: TaskType[] = [
+        {id: v1(), title: 'CSS', isDone: false},
+        {id: v1(), title: 'JS', isDone: true},
+        {id: v1(), title: 'React', isDone: false},
+    ];
+    let tasks2: Array<TaskType> = [
+        {id: v1(), title: 'Terminator', isDone: false},
+        {id: v1(), title: 'XXX', isDone: true},
+        {id: v1(), title: 'Gentlemen of fortune', isDone: false},
+    ];
+    return (
+        <div className="App">
+            <Todolist title={'What to learn'} tasks={tasks1}/>
+            <Todolist title={'What to watch'} tasks={tasks2}/>
+        </div>
+    );
 }
 
 export default App;
